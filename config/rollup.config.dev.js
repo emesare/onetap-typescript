4917fd47006ed4aaedf2c284e93b423b08332025
M0N7Y5 <krystof.montilla@protonmail.com>
2021-08-03 19:55:00 +0200

    added loose flag

--- config/rollup.config.dev.js
+++ config/rollup.config.dev.js
@@ -20,6 +20,7 @@ export default {
                 [
                     "@babel/env",
                     {
+                        "loose": true,
                         "useBuiltIns": "entry",
                         "targets": {
                             "browsers": ["IE 6"]
